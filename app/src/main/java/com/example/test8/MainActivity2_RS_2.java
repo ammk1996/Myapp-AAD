5e4b5674182b36ae0f61611fa6b5c8f5f82c43a6
ammk1996 <ammk1996@users.noreply.github.com>
2020-06-22 06:41:56 +0800

    My app fixed user interface

--- app/src/main/java/com/example/test8/MainActivity2_RS_2.java
+++ app/src/main/java/com/example/test8/MainActivity2_RS_2.java
@@ -2,12 +2,16 @@ package com.example.test8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2_RS_2 extends AppCompatActivity {
     TextView tv1,tv2,tv3,tv4;
     String st1,st2,st3,st4;
+    Button btn_RM1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,28 @@ public class MainActivity2_RS_2 extends AppCompatActivity {
         tv2=findViewById(R.id.tv2);
         tv3=findViewById(R.id.tv3);
         tv4=findViewById(R.id.tv4);
+        btn_RM1 = (Button)findViewById(R.id.btn_RM1);
 
         st1=getIntent().getExtras().getString("Name");
         st2=getIntent().getExtras().getString("CNumber");
         st3=getIntent().getExtras().getString("Phone");
         st4=getIntent().getExtras().getString("DOR");
-        tv1.setText("Hello "+st1);
-        tv2.setText("You've sent table reservation for "+st2+" customers on the date of " + st4 + ".");
+
+        tv2.setText("Hello " + st1 + ", you've sent table reservation for "+st2+" customer(s) on the date of " + st4 + ".");
         tv3.setText("Your phone number is "+ st3 + " ");
         tv4.setText("Once confirmed, you will receive confirmation call from us.");
+        btn_RM1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+
+
+
+    }
+    private void openMP(){
+        Intent intent = new Intent(this,MainActivity.class);
     }
 }
